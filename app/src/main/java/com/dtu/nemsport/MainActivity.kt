f777b84d67c085a66b06a3b24bd982df02999994
Numan Bashir <sportybashir@gmail.com>
2021-10-11 10:38:22 +0200

    Bottom Navigation Bar

--- app/src/main/java/com/dtu/nemsport/MainActivity.kt
+++ app/src/main/java/com/dtu/nemsport/MainActivity.kt
@@ -1,11 +1,28 @@
-package com.dtu.nemsport
+package com.example.bottomnavbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.dtu.nemsport.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Navigation view
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        // Navigation controller
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.profilFragment,R.id.aktiviteterFragment,R.id.banerFragment,R.id.indstillingerFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
