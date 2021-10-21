9a63b2cbc181934629361c533ad8399c5589bb03
Repeller <29373163+Repeller@users.noreply.github.com>
2021-10-21 23:21:03 +0200

    models

---
+++ .netWebApi/nemsport-web-api/Models/Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nemsport_web_api.Models
+{
+    public class Member
+    {
+        // props
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string MemberType { get; set; }
+
+        // constructor
+        public Member()
+        { 
+        }
+
+        public Member(int id, string username, string password, string memberType) 
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            MemberType = memberType;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Username)}: {Username}, {nameof(Password)}: {Password}, {nameof(MemberType)}: {MemberType}";
+        }
+    }
+}
---
+++ .netWebApi/nemsport-web-api/Models/Time.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nemsport_web_api.Models
+{
+    public class Time
+    {
+        int Id { get; set; }
+        DateTime TimeStart { get; set; }
+        DateTime TimeEnd { get; set; }
+
+        public Time() 
+        { 
+        }
+
+        public Time(int id, DateTime timeStart, DateTime timeEnd)
+        {
+            Id = id;
+            TimeStart = timeStart;
+            TimeEnd = timeEnd;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(TimeStart)}: {TimeStart}, {nameof(TimeEnd)}: {TimeEnd}";
+        }
+
+    }
+}
