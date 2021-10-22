c116896e223f049a008aeb59fbcd9eb45351f898
Repeller <29373163+Repeller@users.noreply.github.com>
2021-10-23 00:32:50 +0200

    added all 4 controllers

---
+++ .netWebApi/nemsport-web-api/Controllers/EventController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using nemsport_web_api.Models;
+using nemsport_web_api.DbBuilds;
+
+namespace nemsport_web_api.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        public ManageEvent ManageEvents = new ManageEvent();
+
+        // ctor
+        public EventController()
+        {
+            ManageEvents.Events = ManageEvents.LoadEvents();
+        }
+
+        /// <summary>
+        /// gets all the Event
+        /// </summary>
+        /// <returns>the list of all Event</returns>
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return ManageEvents.Events;
+        }
+
+        // GET: api/Events/5
+        //[HttpGet("{id}", Name = "Get")]
+        /// <summary>
+        /// get one Event by id
+        /// </summary>
+        /// <param name="id">the id of the Event you want</param>
+        /// <returns>the Event you are looking for</returns>
+        [HttpGet]
+        [Route("{id}")]
+        public Event Get(int id)
+        {
+            //Item error = new Item(0, "error", "error", 404.404);
+
+            return ManageEvents.Get(id);
+        }
+
+        // POST: api/Events
+        /// <summary>
+        /// post one Event to the list
+        /// </summary>
+        /// <param name="value">the Event you want to add to the list</param>
+        [HttpPost]
+        public void Post([FromBody] Event value)
+        {
+            ManageEvents.Post(value);
+        }
+
+        // PUT: api/Events/5
+        //[HttpPut("{id}")]
+        /// <summary>
+        /// put/edit one Event's values and replace them with new ones
+        /// </summary>
+        /// <param name="id">the id of the Event to put into</param>
+        /// <param name="value">the value that will be put into the Event</param>
+        [HttpPut]
+        [Route("{id}")]
+        public void Put(int id, [FromBody] Event value)
+        {
+            ManageEvents.Put(id, value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// delete one Event from the list
+        /// </summary>
+        /// <param name="id">the id of the Event you want to delete</param>
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            ManageEvents.Delete(id);
+        }
+
+    }
+}
---
+++ .netWebApi/nemsport-web-api/Controllers/MemberController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using nemsport_web_api.Models;
+using nemsport_web_api.DbBuilds;
+
+namespace nemsport_web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+        public ManageMember ManageMembers = new ManageMember();
+
+        // ctor
+        public MemberController()
+        {
+            ManageMembers.Members = ManageMembers.LoadMembers();
+        }
+
+        /// <summary>
+        /// gets all the Member
+        /// </summary>
+        /// <returns>the list of all Member</returns>
+        [HttpGet]
+        public IEnumerable<Member> Get()
+        {
+            return ManageMembers.Members;
+        }
+
+        // GET: api/Members/5
+        //[HttpGet("{id}", Name = "Get")]
+        /// <summary>
+        /// get one Member by id
+        /// </summary>
+        /// <param name="id">the id of the Member you want</param>
+        /// <returns>the Member you are looking for</returns>
+        [HttpGet]
+        [Route("{id}")]
+        public Member Get(int id)
+        {
+            //Item error = new Item(0, "error", "error", 404.404);
+
+            return ManageMembers.Get(id);
+        }
+
+        // POST: api/Members
+        /// <summary>
+        /// post one Member to the list
+        /// </summary>
+        /// <param name="value">the Member you want to add to the list</param>
+        [HttpPost]
+        public void Post([FromBody] Member value)
+        {
+            ManageMembers.Post(value);
+        }
+
+        // PUT: api/Members/5
+        //[HttpPut("{id}")]
+        /// <summary>
+        /// put/edit one Member's values and replace them with new ones
+        /// </summary>
+        /// <param name="id">the id of the Member to put into</param>
+        /// <param name="value">the value that will be put into the Member</param>
+        [HttpPut]
+        [Route("{id}")]
+        public void Put(int id, [FromBody] Member value)
+        {
+            ManageMembers.Put(id, value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// delete one Member from the list
+        /// </summary>
+        /// <param name="id">the id of the Member you want to delete</param>
+        [HttpDelete]
+        [Route("{id}")]
+        public void Delete(int id)
+        {
+            ManageMembers.Delete(id);
+        }
+    }
+}
