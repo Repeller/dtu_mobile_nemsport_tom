429357762f03b36a4a3c3057955a9b89ed391f19
Alec <Alexranjitkar@gmail.com>
2021-10-26 15:04:19 +0200

    Opret profil side

--- app/src/main/java/com/dtu/nemsport/MainActivity.kt
+++ app/src/main/java/com/dtu/nemsport/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.bottomnavbar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.dtu.nemsport.OpretProfilAktivitet
 import com.dtu.nemsport.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -13,6 +17,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+
+
+        val intent = Intent(this, OpretProfilAktivitet::class.java)
+        startActivity(intent)
+
+        /*val knap: Button = findViewById(R.id.button)
+        knap.setOnClickListener {
+
+
+        }*/
+
 
         // Navigation view
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
