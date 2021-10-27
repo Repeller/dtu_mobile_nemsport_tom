d99f69fdd6a68bff019546a036981be1b3a5fd1b
numanbashir <sportybashir@gmail.com>
2021-10-27 17:25:45 +0200

    Login side færdig

--- app/src/main/java/com/dtu/nemsport/GlemtKode.kt
+++ app/src/main/java/com/dtu/nemsport/GlemtKode.kt
@@ -1,11 +1,20 @@
 package com.dtu.nemsport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GlemtKode : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_glemt_kode)
+
+        val knapTilMainPage: Button = findViewById(R.id.buttonResetPassword)
+        knapTilMainPage.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
