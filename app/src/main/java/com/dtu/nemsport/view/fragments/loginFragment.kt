8bed3055d713ce92d0b94c76381a507e2a0ad467
Alexander Ziegler <29373163+Repeller@users.noreply.github.com>
2022-01-11 01:16:45 +0100

    working on login

--- app/src/main/java/com/dtu/nemsport/view/fragments/loginFragment.kt
+++ app/src/main/java/com/dtu/nemsport/view/fragments/loginFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.Navigation
 import com.dtu.nemsport.R
 import com.dtu.nemsport.view.MainPage
@@ -25,6 +27,13 @@ class loginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    // views and text inputs from the front page
+    private lateinit var input_username: EditText
+    private lateinit var input_password: EditText
+    private lateinit var out_feedback: TextView
+    private var foundUser: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +61,23 @@ class loginFragment : Fragment() {
             Navigation.findNavController(view).navigate(R.id.loginToGlemtKode)
 
         }
+        // 00 - set the views/inputs to our local vars
+        input_username = view.findViewById(R.id.loginWriteEmail)
+        input_password = view.findViewById(R.id.loginWritePassword)
+        out_feedback = view.findViewById(R.id.textView_feedback)
 
+        // 01 - check if the value is in the database
+
+
+        // 01.1 - yes, save id in local values
+        // foundUser = true
+
+        // 01.1.1 - change the page
+
+
+        // 01.2 - no, change feedback
+
+        // TODO: edit this function
         buttonLogin.setOnClickListener {
             requireActivity().run {
                 startActivity(Intent(this, MainPage::class.java))
