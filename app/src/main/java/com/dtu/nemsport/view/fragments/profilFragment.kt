d2dbe721b46dba60ae3016b13f65ed7d4d2c3f94
Alexander Ziegler <29373163+Repeller@users.noreply.github.com>
2022-01-17 13:37:11 +0100

    now the app get the data from the database

--- app/src/main/java/com/dtu/nemsport/view/fragments/profilFragment.kt
+++ app/src/main/java/com/dtu/nemsport/view/fragments/profilFragment.kt
@@ -1,20 +1,20 @@
 package com.dtu.nemsport.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.*
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.navArgs
+import androidx.preference.PreferenceManager
 import com.dtu.nemsport.R
 import com.dtu.nemsport.models.FakeDB
-import com.dtu.nemsport.models.FakeDB.db
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.runBlocking
 
 
 class profilFragment : Fragment() {
@@ -26,7 +26,6 @@ class profilFragment : Fragment() {
     lateinit var navn: TextView
     lateinit var bigNavnProfil: TextView
     private lateinit var db: FirebaseFirestore
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,22 +40,72 @@ class profilFragment : Fragment() {
         val nyAdresse = view.findViewById<TextView>(R.id.adresse)
         val nyNummer = view.findViewById<TextView>(R.id.nummer)
 
-
         // Set each variable as the text from the FakeDB
-        navn.text = FakeDB.userData[0].navn
-        bigNavnProfil.text = FakeDB.userData[0].navn
-        nyEmail.text = FakeDB.userData[0].email
-        nyAdresse.text = FakeDB.userData[0].adresse
-        nyNummer.text = FakeDB.userData[0].nummer
+//        navn.text = FakeDB.userData[0].navn
+//        bigNavnProfil.text = FakeDB.userData[0].navn
+//        nyEmail.text = FakeDB.userData[0].email
+//        nyAdresse.text = FakeDB.userData[0].adresse
+//        nyNummer.text = FakeDB.userData[0].nummer
 
         return view
 
+    }
+
+    suspend fun getAndSetData(){
+        db = FirebaseFirestore.getInstance()
+
+        // 00 - get user id
+        var sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        var uid = sharedPref.getString("nemsport_uid", "")
+
+        if (uid != null) {
+            Log.w("uid - profil - onViewCreated:", uid)
+        }
+        // 00 - get the values from the DB
+        if (uid != null) {
+
+            navn = requireView().findViewById(R.id.navn)
+            bigNavnProfil = requireView().findViewById(R.id.bigNavnProfil)
+            val nyEmail = requireView().findViewById<TextView>(R.id.email)
+            val nyAdresse = requireView().findViewById<TextView>(R.id.adresse)
+            val nyNummer = requireView().findViewById<TextView>(R.id.nummer)
+
+            db.collection("users").document(uid)
+                .get()
+                .addOnSuccessListener { doc ->
+                    navn.text = doc.getString("name")
+                    bigNavnProfil.text = doc.getString("name")
+                    nyEmail.text = doc.getString("mail")
+                    nyAdresse.text = doc.getString("address")
+                    nyNummer.text = doc.getString("phone")
+                }
+                .addOnFailureListener {
+                    // TODO: maybe we need to do it some other way than this xDDDDD
+                    navn.text = "error"
+                    bigNavnProfil.text = "error"
+                    nyEmail.text = "error"
+                    nyAdresse.text = "error"
+                    nyNummer.text = "error"
+                }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         skiftIndstillinger = view.findViewById(R.id.skiftIndstillinger)
+
+        // 00 - get user id
+        var sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        var uid = sharedPref.getString("nemsport_uid", "")
+
+        if (uid != null) {
+            Log.w("uid - profil - onViewCreated:", uid)
+        }
+
+        runBlocking {
+            getAndSetData()
+        }
 
         // Navigate to EditProfile fragment
         skiftIndstillinger.setOnClickListener {
