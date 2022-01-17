23b73ed3306ec8ccd943444c1f44f6d44c0d1d44
numanbashir <sportybashir@gmail.com>
2022-01-17 20:35:02 +0100

    update profile data

--- app/src/main/java/com/dtu/nemsport/models/User.kt
+++ app/src/main/java/com/dtu/nemsport/models/User.kt
@@ -3,7 +3,7 @@ package com.dtu.nemsport.models
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class User(val mail: String? = null, val name: String? = null, val phone: String? = null, val address: String? = null, val member: String? = null) {
+data class User(val mail: String? = null, val name: String? = null, val phone: String? = null, val address: String? = null, val member: Boolean? = null) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
 }
